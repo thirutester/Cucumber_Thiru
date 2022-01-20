d7bc9144be50a582a490bac8742cc4362367161a
thirutester <m.thiruvazhagan@gmail.com>
2022-01-20 21:53:54 +0530

    First day task

---
+++ src/test/java/com/flipkart/stepdefinition/MobilePurchaseSteps.java
@@ -0,0 +1,69 @@
+package com.flipkart.stepdefinition;
+
+import java.util.Set;
+
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+
+public class MobilePurchaseSteps {
+	static WebDriver d;
+	@Given("user launches flipkart application")
+	public void user_launches_flipkart_application() {
+			WebDriverManager.chromedriver().setup();
+	        d = new ChromeDriver();
+	        d.manage().window().maximize();
+	        d.get("https://www.flipkart.com/");
+		
+	}
+
+	@Given("user login by entering crendentials")
+	public void user_login_by_entering_crendentials() {
+		try {
+	   		WebElement button = d.findElement(By.xpath("//button[@class='_2KpZ6l _2doB4z']"));
+	   		 button.isDisplayed();
+	   	     button.click();
+	   	}catch (Exception e) {
+	   		System.out.println("Popup not displayed");
+	   	}
+	}
+
+	@When("user search mobile")
+	public void user_search_mobile() throws Throwable {
+		WebElement search = d.findElement(By.xpath("//input[@type='text']"));
+	  	search.sendKeys("realme",Keys.ENTER);
+	    Thread.sleep(2000);
+	  	WebElement mobileName = d.findElement(By.xpath("(//div[@class='_4rR01T'])[2]"));
+	    Thread.sleep(2000);
+		//String name = mobileName.getText();
+		//System.out.println(name);
+		mobileName.click();
+	  	
+	}
+
+	@When("user click on the mobile name")
+	public void user_click_on_the_mobile_name() {
+	  	String par = d.getWindowHandle();
+	  	Set<String> child = d.getWindowHandles();
+			for(String x : child) {
+			if(!par.equals(x)) {
+					System.out.println("tab switched");
+					d.switchTo().window(x);
+				}
+			}
+	}
+
+	@Then("user click on add to cart")
+	public void user_click_on_add_to_cart() {
+		//Assert.assertTrue(d.findElement(By.xpath("//button[@class='_2KpZ6l _2U9uOA ihZ75k _3AWRsL']")).isDisplayed());
+	}
+}
---
+++ target/test-classes/Features/Mobile_purchase.feature
@@ -0,0 +1,9 @@
+#Author: your.email@your.domain.com
+Feature: Mobile Purchase in flipkart application
+  
+ Scenario: Buy Mobile
+ Given user launches flipkart application
+ And user login by entering crendentials
+ When user search mobile
+ And user click on the mobile name
+ Then user click on add to cart
